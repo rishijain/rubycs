3c5943bf4d58609ea8415251cb7f52811903927a
rishijain <jainrishi.37@gmail.com>
2019-11-22 09:11:48 +0530

    insert elements to bst tree

---
+++ bst.rb
@@ -0,0 +1,44 @@
+# insert elements in a bst
+#
+
+class Node
+  attr_accessor :left, :right, :data
+  def initialize(data)
+    @data = data
+  end
+
+  def print_node
+    "#{@left} - #{@data} - #{@right}"
+  end
+end
+
+
+class Bst
+  attr_accessor :root
+
+  def initialize
+    @root = nil
+  end
+
+  def insert_node(tmp_root, data)
+    new_node = Node.new data
+    if tmp_root.nil?
+      tmp_root = new_node
+      @root = tmp_root if @root.nil?
+    else
+      if data <= tmp_root.data
+        if tmp_root.left.nil?
+          tmp_root.left = new_node
+        else
+          insert_node(tmp_root.left, data)
+        end
+      else
+        if tmp_root.right.nil?
+          tmp_root.right = new_node
+        else
+          insert_node(tmp_root.right, data)
+        end
+      end
+    end
+  end
+end
